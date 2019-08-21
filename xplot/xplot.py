dfa7e7ea0724ea7a79ddcdd05051b4980e103dff
mariia <sssagitta@gmail.com>
2019-08-21 13:58:54 +0200

    Storing xpoints separately from xplot.py

--- xplot/xplot.py
+++ xplot/xplot.py
@@ -1,4 +1,5 @@
 from myplot import *
+from xpoints import *
 import numpy as np
 import pandas as pd
 
@@ -19,12 +20,3 @@ class XPlot(Plot):
         for ax in axes:
             for x in xlist:
                 ax.axvline(pd.to_datetime(x), linestyle='--', label = XP[x][0], zorder=4, color=XP[x][1], linewidth=2)
-
-
-
-# X points dictionary = {x point : [label, color]}
-XP = {
-'2018-03-14': ['Pi day','r'],
-'2017-12-25': ['25 dec = 31 oct', 'b'],
-'2018-07-22': ['Pi approximation day', 'g']
-}
---
+++ xplot/xpoints.py
@@ -0,0 +1,10 @@
+# X points dictionary = {x point : [label, color]}
+
+# example:
+XP = {
+'2018-03-14': ['Pi day','r'],
+'2017-12-25': ['25 dec = 31 oct', 'b'],
+'2018-07-22': ['Pi approximation day', 'g']
+}
+
+# store it in the file xpoints.py for specific use, not in xplot.py itself
