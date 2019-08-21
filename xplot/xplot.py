d5802db6febbd0f9af62812b26516925d5a4c6b9
mariia <sssagitta@gmail.com>
2019-08-21 13:54:27 +0200

    Added more functionality to XPlot

--- xplot/xplot.py
+++ xplot/xplot.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 class XPlot(Plot):
-    def xpoints(self,xlim=None):
+    def xpoints(self, xlim=None, axis='all'):
         '''
         Plot special points as vertial lines.
         Intended for time series, but can be modified for any x axis (just remove pd.to_datetime)
@@ -15,7 +15,8 @@ class XPlot(Plot):
         # select only the ones in our region
         if xlim: xlist = xlist[xlist > xlim]
         # plot
-        for ax in self.axes:
+        axes = self.axes if axis == 'all' else [self.axes[axis]]
+        for ax in axes:
             for x in xlist:
                 ax.axvline(pd.to_datetime(x), linestyle='--', label = XP[x][0], zorder=4, color=XP[x][1], linewidth=2)
 
